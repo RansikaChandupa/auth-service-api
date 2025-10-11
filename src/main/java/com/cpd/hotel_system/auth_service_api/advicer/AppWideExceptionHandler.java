8df0e849e2a48545e78b9df0beb342de3db6f64b
RansikaChandupa <ransikachandupa@gmail.com>
2025-10-11 19:04:59 +0530

    Service implementation for system user,standard response dto, app wide exception handler and configurations to access keycloak server done

---
+++ src/main/java/com/cpd/hotel_system/auth_service_api/advicer/AppWideExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.cpd.hotel_system.auth_service_api.advicer;
+
+import com.cpd.hotel_system.auth_service_api.BadRequestException;
+import com.cpd.hotel_system.auth_service_api.util.StandardResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class AppWideExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<StandardResponseDto> handleBadRequestException(BadRequestException ex) {
+        return new ResponseEntity<StandardResponseDto>(
+                new StandardResponseDto(400,ex.getMessage(),ex),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+}
